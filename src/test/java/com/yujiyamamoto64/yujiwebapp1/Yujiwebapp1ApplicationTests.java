60afffdaecfc03410bbbdc4a5287d159fbc41105
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-30 11:59:02 -0300

    first commit

---
+++ src/test/java/com/yujiyamamoto64/yujiwebapp1/Yujiwebapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.yujiwebapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yujiwebapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
