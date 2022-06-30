cdc2a1af38de59c4efe90452d4281478c5cc7bf5
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-30 12:54:38 -0300

    index page

--- src/main/java/com/yujiyamamoto64/yujiwebapp1/Yujiwebapp1Application.java
+++ src/main/java/com/yujiyamamoto64/yujiwebapp1/Yujiwebapp1Application.java
@@ -2,19 +2,9 @@ package com.yujiyamamoto64.yujiwebapp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class Yujiwebapp1Application {
-	
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!  modified: 22063012h25";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Yujiwebapp1Application.class, args);
